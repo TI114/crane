1269688ba52299b11aedfc0a3e586cb8e036b680
Tim Meusel <tim@bastelfreak.de>
2018-01-06 18:27:36 +0100

    add foo

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using static OpenTK.Graphics.OpenGL.GL;
+using MatrixMode = OpenTK.Graphics.OpenGL.MatrixMode;
+
+namespace Crane
+{
+   class Game : GameWindow
+    {
+        public Game()
+            : base(800, 600, GraphicsMode.Default, "OpenTK Quick Start Sample")
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
+            Enable(EnableCap.DepthTest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+            MatrixMode(MatrixMode.Projection);
+            LoadMatrix(ref projection);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (Keyboard[Key.Escape])
+                Exit();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+            MatrixMode(MatrixMode.Modelview);
+            LoadMatrix(ref modelview);
+
+            Begin(BeginMode.Triangles);
+
+            Color3(1.0f, 1.0f, 0.0f);
+            Vertex3(-1.0f, -1.0f, 4.0f);
+            Color3(1.0f, 0.0f, 0.0f);
+            Vertex3(1.0f, -1.0f, 4.0f);
+            Color3(0.2f, 0.9f, 1.0f);
+            Vertex3(0.0f, 1.0f, 4.0f);
+
+            End();
+
+            SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main2()
+        {
+            // The 'using' idiom guarantees proper resource cleanup.
+            // We request 30 UpdateFrame events per second, and unlimited
+            // RenderFrame events (as fast as the computer can handle).
+            using (Game game = new Game())
+            {
+                game.Run(30.0);
+            }
+        }
+    }
+}
